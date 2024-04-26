fae7c9859ef89f33e1b03e39d609fe5649537484
saamonlover <saamonlover@gmail.com>
2024-04-25 17:10:19 -0700

    feat: log launch message when bot is started (the register commands file should be in the previous push)

---
+++ src/events/ready/01-register-commands.js
@@ -0,0 +1,51 @@
+const { testServer } = require('../../../config.json')
+const areCommandsDifferent = require('../../utils/are-commands-different')
+const getApplicationCommands = require('../../utils/get-application-commands')
+const getLocalCommands = require('../../utils/get-local-commands')
+
+module.exports = async (client) => {
+  try {
+    const localCommands = getLocalCommands()
+    const applicationCommands = await getApplicationCommands(client, testServer)
+
+    for (const localCommand of localCommands) {
+      const { name, description, options } = localCommand.data
+      const existingCommand = await applicationCommands.cache.find(
+        (cmd) => cmd.name === name,
+      )
+      if (existingCommand) {
+        if (localCommand.data.deleted) {
+          await applicationCommands.delete(existingCommand.id)
+          console.log(`> [][log] deleted command: "${name}".`)
+          continue
+        }
+        if (areCommandsDifferent(existingCommand, localCommand.data)) {
+          await applicationCommands.edit(existingCommand.id, {
+            description,
+            options,
+          })
+          console.log(`> [][log] edited command: "${name}".`)
+        }
+      } else {
+        if (localCommand.data.deleted) {
+          console.log(
+            `> [note]: skipping registering command "${name}" as it's set to deleted.`,
+          )
+          continue
+        }
+        await applicationCommands.create({ name, description, options })
+        console.log(`> [update]: registered command: "${name}".`)
+      }
+    }
+
+    /* // Clear all global commands
+    client.application.commands
+      .set([])
+      .then(() => console.log('> [] cleared all global commands'))
+      .catch((err) =>
+        console.error(`> [err]: error clearing global commands: ${err}`),
+      ) */
+  } catch (error) {
+    console.error(`> [err]: there was an error ${error}`)
+  }
+}
---
+++ src/events/ready/at-launch.js
@@ -0,0 +1,3 @@
+module.exports = (client) => {
+  console.log(`> [ready]: bot ${client.user.tag} is online`)
+}
