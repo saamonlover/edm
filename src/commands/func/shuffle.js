28af26276a92f3ec973945465f5fe1cd0a5b8b31
saamonlover <saamonlover@gmail.com>
2024-05-01 23:06:45 -0700

    chore: adjust description of the command

---
+++ src/commands/func/shuffle.js
@@ -0,0 +1,9 @@
+const { EmbedBuilder } = require('discord.js')
+
+module.exports = {
+  callback: async (_, interaction) => {},
+  data: {
+    name: 'shuffle',
+    description: 'Shuffle the current queue',
+  },
+}
