146e26d5116b9ab7ad0a25702d039254c7bbf01e
saamonlover <saamonlover@gmail.com>
2024-05-01 23:01:44 -0700

    fix: avoid player racing by removing the player acitivity in the skip (note that the player stop operation will auto stop playing the current track and switch to the next item)

--- src/commands/func/skip.js
+++ src/commands/func/skip.js
@@ -1,14 +1,7 @@
 const { EmbedBuilder } = require('discord.js')
-const {
-  joinVoiceChannel,
-  createAudioPlayer,
-  createAudioResource,
-  NoSubscriberBehavior,
-} = require('@discordjs/voice')
+const { joinVoiceChannel, createAudioResource } = require('@discordjs/voice')
 
 const YouTube = require('youtube-sr').default
-
-const play = require('play-dl')
 
 module.exports = {
   callback: async (_, interaction) => {
@@ -20,67 +13,20 @@ module.exports = {
     }
 
     // Head to the next track
-    /* global.player.stop() */
+    global.player.stop()
 
-    // Defer interaction reply
-    await interaction.deferReply()
-
-    // Play all tracks added
-    while (global.tracks.length > 0) {
-      // Get the next track
-      const track = global.tracks.shift()
-      const trackUrl = track.external_urls.spotify
-      const trackName = track.name
-      const artistName = track.artists[0].name
-
-      // Search on YouTube Music
-      const searchQuery = `${trackName} ${artistName} audio`
-      const ytSearchResult = await YouTube.search(searchQuery, {
-        limit: 1,
-        safeSearch: true,
-      })
-      const ytTrackUrl = `https://music.youtube.com/watch?v=${ytSearchResult[0].id}`
-
-      // Check if the bot is in a voice channel
-      if (!global.connection) {
-        // Join the voice channel
-        const channel = interaction.member.voice.channel
-        if (!channel) {
-          return interaction.editReply(
-            'You must be in a voice channel to play music!',
-          )
-        }
-        global.connection = joinVoiceChannel({
-          channelId: channel.id,
-          guildId: channel.guild.id,
-          adapterCreator: channel.guild.voiceAdapterCreator,
-        })
-      }
-
-      // Play the track
-      const stream = await play.stream(ytTrackUrl)
-      const resource = createAudioResource(stream.stream, {
-        inputType: stream.type,
-      })
-      global.player = createAudioPlayer({
-        behaviors: {
-          noSubscriber: NoSubscriberBehavior.Play,
-        },
-      })
-      global.player.play(resource)
-      global.connection.subscribe(global.player)
-
-      // Interaction reply
-      const embed = new EmbedBuilder()
-        .setDescription(
-          `Now playing [${trackName} - ${artistName}](${trackUrl})`,
-        )
-        .setColor('#FF0000')
-      await interaction.editReply({ embeds: [embed] })
-
-      // Wait for the song to finish
-      await new Promise((resolve) => global.player.on('idle', resolve))
-    }
+    // Interaction reply
+    const embed = new EmbedBuilder()
+      .setDescription(`Current song skipped`)
+      .setColor('#FF0000')
+    const embedMessage = await interaction.reply({
+      embeds: [embed],
+      fetchReply: true,
+    })
+    // Delete the embed message after 5 seconds
+    setTimeout(() => {
+      embedMessage.delete()
+    }, 5000)
   },
   data: {
     name: 'skip',
