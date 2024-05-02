8a7635f91044d8457afb3f004b9acb8552d5d028
saamonlover <saamonlover@gmail.com>
2024-05-01 23:14:26 -0700

    feat: enable queue shuffling

--- src/commands/func/shuffle.js
+++ src/commands/func/shuffle.js
@@ -1,7 +1,29 @@
 const { EmbedBuilder } = require('discord.js')
 
 module.exports = {
-  callback: async (_, interaction) => {},
+  callback: async (_, interaction) => {
+    // Shuffle the queue using the Fisher-Yates (Knuth) shuffle algorithm
+    for (let i = global.tracks.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[global.tracks[i], global.tracks[j]] = [
+        global.tracks[j],
+        global.tracks[i],
+      ]
+    }
+
+    // Interaction reply
+    const embed = new EmbedBuilder()
+      .setDescription(`Queue shuffled`)
+      .setColor('#FF0000')
+    const embedMessage = await interaction.reply({
+      embeds: [embed],
+      fetchReply: true,
+    })
+    // Delete the embed message after 5 seconds
+    setTimeout(() => {
+      embedMessage.delete()
+    }, 5000)
+  },
   data: {
     name: 'shuffle',
     description: 'Shuffle the current queue',
