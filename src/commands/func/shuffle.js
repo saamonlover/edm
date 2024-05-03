cd68224bf584306d891d2f5d7cc2fa5fe028db6d
saamonlover <saamonlover@gmail.com>
2024-05-03 14:22:12 -0700

    fix: prevent from being disconnected due to unknown reasons by removing the next playing song and add it to a random position in the shuffled queue

--- src/commands/func/shuffle.js
+++ src/commands/func/shuffle.js
@@ -2,7 +2,10 @@ const { EmbedBuilder } = require('discord.js')
 
 module.exports = {
   callback: async (_, interaction) => {
-    // Shuffle the queue using the Fisher-Yates (Knuth) shuffle algorithm
+    // Remove the next playing track from the queue
+    const currentTrack = global.tracks.shift()
+
+    // Shuffle the rest of the queue using the Fisher-Yates (Knuth) shuffle algorithm
     for (let i = global.tracks.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1))
       ;[global.tracks[i], global.tracks[j]] = [
@@ -10,6 +13,12 @@ module.exports = {
         global.tracks[i],
       ]
     }
+
+    // Generate a random index
+    const randomIndex = Math.floor(Math.random() * global.tracks.length)
+
+    // Add the next playing track back at a random position in the queue
+    global.tracks.splice(randomIndex, 0, currentTrack)
 
     // Interaction reply
     const embed = new EmbedBuilder()
